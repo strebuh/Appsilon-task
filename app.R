a980e69bc6c94d62466eef133e9601a92c6aa2bd
strebuh <hubert.smieciuch@gmail.com>
2021-05-18 19:31:21 +0200

    work on inforbox pt1

--- app.R
+++ app.R
@@ -6,35 +6,13 @@ source("mod_max_dist.R")
 library(shiny)
 app_ui <- function() {
   semanticPage(
-    # Call the UI  function, this is the only place 
-    # your ids will need to be unique
-    
-    # modules
+    title = "Ship max distance",
     vessels_ui(id = "v_type_id", note=TRUE)#,
-    
-    # # modules2
-    # vessels_ui(id = "v_type_id", v_type=TRUE, note=FALSE),
-    # vessels_ui(id = "v_name_id", v_type=FALSE, note=TRUE)
-    
-    # fluidRow(
-    #   vessels_ui(id = "v_type_id", type="v_type", note=FALSE)
-    # ),
-    # fluidRow(
-    #   vessels_ui(id = "v_name_id", type="v_name", note=TRUE)
-    # )
   )
 }
 
 app_server <- function(input, output, session) {
-  # We are now calling the module server functions 
-  # on a given id that matches the one from the UI
-  
-  # modlues
   vessels_server(id = "v_type_id")
-  
-  # # modules2
-  # vessels_server(id = "v_type_id", v_type=TRUE)
-  # vessels_server(id = "v_name_id", v_type=FALSE)
 }
 
 shinyApp(app_ui, app_server)
--- mod_max_dist.R
+++ mod_max_dist.R
@@ -1,70 +1,145 @@
 library(shiny)
 library(shiny.semantic)
+library(shinydashboard)
 library(leaflet)
+library(data.table)
 
 
-# browser()
-# getwd()
 vessel_types <- readRDS("data/vessel_types.RDS")
-data <- readRDS("data/prep_data.RDS")
+data_by_type <- readRDS("data/data_by_type.RDS")
+data_by_country <- readRDS("data/data_by_country.RDS")
+data_by_ship <- readRDS("data/data_by_ship.RDS")
+
+
+info_box <- function(info, icon, color){
+  div(class = paste0("ui icon", color," message"),
+      icon(icon),
+      div(class = "content",
+          div(class = "header", p(info))
+      )
+  )
+}
+
+get_stat <- function(data, column, stat, ...) {
+  if(column %in% names(data)){
+    stat <- do.call(stat, list(data[[column]]))
+    return(stat)
+  } else {
+    return(NA)
+  }
+}
+
 
 # Module UI function
 vessels_ui <- function(id, note) {
-  # `NS(id)` returns a namespace function, which was save as `ns` and will
-  # invoke later.
   ns <- NS(id)
-  
   tagList(
-    selectInput(ns("v_type_imt"), 
-                  "Vessel types",
-                  choices = vessel_types,
-                  selected = vessel_types[1]),
-    shiny::uiOutput(ns("v_imt_UI")),
-    if(note == T){
-      h3(htmlOutput(ns("v_note_opt")))
-    },
-    leafletOutput(ns("v_move_map"))
+    sidebar_layout(
+      sidebar_panel(
+        selectInput(ns("v_type_imt"), 
+                    "Vessel types",
+                    choices = vessel_types,
+                    selected = vessel_types[1]),
+        br(),
+        uiOutput(ns("v_imt_UI")),
+        br(),
+        info_box(textOutput(
+          ns("n_type_ships")),
+          icon = "ship",
+          color = "green"),
+        br(),
+        info_box(textOutput(
+          ns("n_avg_speed")),
+          icon = "anchor",
+          color = "green"),
+        br(),
+        # info_box(textOutput(
+        #   ns("n_type_ships")),
+        #   icon = "ship",
+        #   color = "green"),
+        # br(),
+        
+        width = 4,
+        fluid = T
+      ),
+      
+    main_panel(
+      h3(htmlOutput(ns("v_note_opt"))),
+      leafletOutput(ns("v_move_map")),
+      width = 8,
+      fluid = T
+    )
+  )
     )
 }
 
+# total distance of the ship
+# average distance of the ship
+# avg speed compared to avg in the group
+# size of the boat comparing to the average in a group
+
 vessels_server <- function(id) {
-  # Calling the moduleServer function
   moduleServer(
-    # Setting the id
     id,
-    # Defining the module core mechanism
     function(input, output, session) {
-      # This part is the same as the code you would put 
-      # inside a standard server
       
+      # Retrieve from the data ships of selected type
       output$v_imt_UI <- renderUI({
         ns <- NS(id)
         req(input$v_type_imt)
-        # browser()
-        choices <- data[ship_type == input$v_type_imt,]$SHIPNAME
+        choices <- data_by_ship[ship_type == input$v_type_imt,]$SHIPNAME
         selectInput(ns("v_imt"),
-                           "Vessel name",
-                           choices = choices,
-                           selected = choices[1])
+                    "Vessel name",
+                    choices = choices,
+                    selected = choices[1])
       })
       
+      
+      output$n_type_ships <- renderText({
+        value = get_stat(data_by_ship[ship_type == input$v_type_imt,], "SHIP_ID", uniqueN)
+        paste("Number of ships:", value)
+        # get_stat(data_by_ship[ship_type == input$v_type_imt,], "id", uniqueN)
+      })
+      
+      output$n_avg_speed <- renderText({
+        req(input$v_imt)
+        
+        name = data_by_ship[SHIPNAME==input$v_imt, ]$SHIPNAME
+        country = data_by_ship[SHIPNAME==input$v_imt, ]$FLAG
+        max_speed = data_by_ship[SHIPNAME==input$v_imt, ]$MAX_SPEED 
+        avg_speed = data_by_ship[SHIPNAME==input$v_imt, ]$AVG_SPEED
+        tot_dist = data_by_ship[SHIPNAME==input$v_imt, ]$TOT_DIST 
+        stand_time = data_by_ship[SHIPNAME==input$v_imt, ]$STAND_TIME
+        
+        paste("Ship name:", name, "\n",
+              "Ship country:", country, "\n", 
+              "Max speed:", max_speed, "\n",
+              "Average speed:", avg_speed, "\n",
+              "Total distance:", tot_dist, "\n",
+              "Standing time:", stand_time)
+      })
+      
+      # Generate and render the note with the distance
       get_note <- reactive({
         req(input$v_type_imt, input$v_imt)
-        paste(input$v_imt, "-", round(data[ship_type == input$v_type_imt & SHIPNAME == input$v_imt,]$DIST_M, 2), "meters.")
+        
+        distance =  round(data_by_ship[ship_type == input$v_type_imt & SHIPNAME == input$v_imt,]$DIST_M, 2)
+        start = data_by_ship[ship_type == input$v_type_imt & SHIPNAME == input$v_imt,]$DATETIME
+        stop = data_by_ship[ship_type == input$v_type_imt & SHIPNAME == input$v_imt,]$DATETIME2
+        
+        paste(input$v_imt, "-", distance, "meters between", start ,"and" , stop)
       })
       output$v_note_opt <- renderText(get_note())
       
+      # Generate leaflet map
       get_map <- reactive({
         req(input$v_type_imt, input$v_imt)
-        leaflet(data = data[ship_type == input$v_type_imt & SHIPNAME == input$v_imt]) %>% 
+        leaflet(data = data_by_ship[ship_type == input$v_type_imt & SHIPNAME == input$v_imt]) %>% 
           addTiles() %>% 
           addMarkers(~LON, ~LAT, label = ~paste("Start stamp at ", as.character(DATETIME))) %>% 
           addMarkers(~LON2, ~LAT2,  label = ~paste("Stop stamp at", as.character(DATETIME2), "after", as.character(difftime(DATETIME2, DATETIME)), "sec."))
       })
-      
       output$v_move_map <- renderLeaflet(get_map())
-
-      
       
     }
   )
